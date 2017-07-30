1d0fdf17ab889065055484e32775fd26b56a2795
Hédi Sellami <cydouzo@gmail.com>
2017-07-30 18:53:15 +0900

    Movement Behavior

---
+++ LudumDare39/Assets/Scripts/BoardHandler/Character.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : Movement {
+	
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	public bool Move (Position destination){
+		bool output = GetComponent<Movement> ().MoveTo (destination);
+		if (!output) {
+			output = PushTo (destination);
+		}
+		return output;
+	}
+	public bool PushTo(Position destination){
+		return false; //TODO
+	}
+
+
+}
---
+++ LudumDare39/Assets/Scripts/BoardHandler/Movement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+	public Position p = new Position(0,0);
+
+	public bool MoveTo (Position i){
+		if (!p.IsNeighbor (i)) {
+			Debug.Log ("Deplace à une position non adjascente");
+			return false; // Ce false mene a d'autres bugs. Fais gaffe!
+		} else if (BoardHandler.instance.FreeTile (i)) {
+			p = i;
+			return true;
+		}
+		return false;
+		//TODO
+	}
+
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
